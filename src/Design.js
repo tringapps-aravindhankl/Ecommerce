7a73a01a443050cb47a6616e6cc65b89c6e6afd9
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-06-20 14:50:15 +0530

    Update Design.js

--- src/Design.js
+++ src/Design.js
@@ -5,7 +5,7 @@ function Design({count}){
         <>
     <div className="whole-header">
         <div className="first-Grid">
-            <h1>Shopper Stop</h1>
+            <h1>Shirt store</h1>
         </div>
         <div className="second-Grid">
             <Link to="/">
